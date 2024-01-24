05b7705cd90ccd6cce668782d15b9d712c98b3ed
Vaibhav <khulbevaibhavdev@gmail.com>
2024-01-24 13:21:14 +0530

    Initial commit with switch component 🚀

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Advanced Radix UI",
+  description:
+    "Radix UI components with Next.js, Tailwind CSS and Framer Motion",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import SwitchComponent from "./components/switch/page";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-gray-900">
+      <SwitchComponent />
+    </main>
+  );
+}
